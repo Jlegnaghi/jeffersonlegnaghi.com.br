95cf0532b77361189d055be48667cf974767f338
Jefferson Legnaghi <51827484+Jlegnaghi@users.noreply.github.com>
2023-05-27 20:24:21 -0300

    Delete web directory

--- web/README.md
+++
@@ -1,2 +0,0 @@
-# jeffersonlegnaghi.com
-Site em construção 
--- web/noindex.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: http://www.uolhost.uol.com.br/avisos-plataforma/aguardando-publicacao.html"); /* Redirect browser */
-exit();
-?>
